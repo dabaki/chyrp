e959619bcd41dee2e21aa48849015ecffd97126f
dabaki <arberbakija@gmail.com>
2011-12-07 00:06:22 +0100

    Fixed in english :)
    Fixed the Like and Unlike module.
    
    Signed-off-by: dabaki <arberbakija@gmail.com>

--- modules/likes/javascript.php
+++ modules/likes/javascript.php
@@ -8,23 +8,23 @@
     $(function() {
 <<<<<<< HEAD
     
-    var lajke = false;
-    var unlajke = false; 
-    var labeli = "Like";
+    var likes = false;
+    var unlikes = false; 
+    var likelabel = "Like";
     
         $(".likepost").click(function() {
 
 			var id = $(this).attr("id").replace(/post_id_/, "");
             var dataString = 'post_id='+ id +'&ajax=true';
-            if (lajke == false) { // Nese useri ska bo Like akoma
-            lajke = true;
-            unlajke=false;
+            if (likes == false) { // before the user likes the post
+            likes = true;
+            unlikes=false;
             labeli="Unlike";
             var fullUrl = '<?php echo Config::current()->chyrp_url; ?>/?action=like'
             }
-            else if (unlajke == false) {  // Nese useri ka bo Like, dhe shfaqet Unlike
-            lajke = false;
-            unlajke=true;
+            else if (unlikes == false) {  // after the user likes the post
+            likes = false;
+            unlikes=true;
             labeli="Like";
             var fullUrl = '<?php echo Config::current()->chyrp_url; ?>/?action=unlike'
            	}
@@ -47,7 +47,7 @@
                     success: function(html) {
 <<<<<<< HEAD
                         
-                        parent.html("("+html.total_likes+") "+labeli);                      
+                        parent.html("("+html.total_likes+") "+likelabel);                      
 =======
                         parent.html(html);
 >>>>>>> 94fdf8728ee90605f72a58d457e90ba2374dc5cd
